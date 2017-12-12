f850b96bd4d6649ab5c0811d44c13fd9634d9d26
ShengChangJian <414834682@qq.com>
2017-12-12 12:05:07 +0800

    at 2017/12/12 周二 12:05:01.56

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -34,7 +34,7 @@
 
 $("figure").each(function(index)
      {
-       $(this).before('<input type="button" title="若显示不正常，请再次点击！" onclick="runCode();"  class="copyButton" value="代码窗口" id="copy_' + index + '" style="margin-left:4em;color:blue;"/>');
+       $(this).before('<input type="button" title="最多允许同时打开4个代码窗口！" onclick="runCode();"  class="copyButton" value="代码窗口" id="copy_' + index + '" style="margin-left:4em;color:blue;"/>');
        $(this).before('<input type="button" title="最多允许同时打开4个代码窗口！" onclick="foldCode();"  class="copyButton" value="展开代码" id="fold_'+ index + '" style="margin-left:1em;color:blue;"/>');
        $(this).attr("id","code_"+index);
      });
@@ -53,28 +53,13 @@ function runCode(){
   var copy=v_id.replace("copy_","code_");
   if(!codeValue)
   {
-    codeValue ='<!DOCTYPE html ><html><head>';
-    codeValue +='<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">';
-    codeValue +='<title>代码窗口</title>';
-    codeValue +='<link rel="stylesheet" href="/assets/js/google-code-prettify/prettify.css">';
-    codeValue +='<style>code{width: 100%; margin: 0; padding-top: 10px; }body{font-size: 1em;}</style>';
-    codeValue +='<style>li{font-size:1.2em;border-left:2px solid green;text-indent: 1em;} li.L0, li.L1, li.L2, li.L3,li.L5, li.L6, li.L7, li.L8{ list-style-type: decimal !important }</style>'; 
-
-    if(window.screen.width <= 770){
-        codeValue+='<style> html{overflow: auto !important; -webkit-overflow-scrolling: touch !important;}li{font-size: 1em;} *{margin: 0; padding: 0;} ol.linenums{ padding-top: 1em; padding-left: 2.2em; width: 100%; height: 100%;}</style>';
-        codeValue+= '</head><body style=" background-color: #2F4F4F;font-size: 1em; position: absolute;">' + $('#'+copy).html();
-        
-    }else{
-        codeValue+= '</head><body onload="PR.prettyPrint()" style="position: absolute;">' + $('#'+copy).html();
-        
-    }else{
-        codeValue += '</head><body onload="PR.prettyPrint()">' + $('#'+copy).html();
-    }
+    codeValue = $('#'+copy).html();
   }
-  var rng = window.open('','codeWin_'+i, 'height=400, width=780, top=100,left=100, toolbar=no, menubar=no, scrollbars=no, resizable=yes,location=no,status=no');
+  var rng = window.open('','codeWin_'+i, 'height=400, width=700, top=100,left=100, toolbar=no, menubar=no, scrollbars=no, resizable=yes,location=no,status=no');
       rng.opener=null;
-      codeValue+='<script src="/assets/js/google-code-prettify/prettify.js">'+'</'+'script>';
-      codeValue+='</body></html>';
+      codeValue+='<style>li{font-size:1.2em;border-left:2px solid green;text-indent: 1em;}li.L0, li.L1, li.L2, li.L3,li.L5, li.L6, li.L7, li.L8{ list-style-type: decimal !important }</style>';
+      codeValue+='<script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js">'+'</'+'script>';
+      rng.document.write("<title>代码窗口</title>");
       rng.document.write(codeValue);
       rng.document.close();
       i++;
@@ -110,40 +95,5 @@ function foldCode(){
               } 
 };
 
-<!--代码窗口和代码折叠结束-->
-
-$('figure').hide();
-
-function foldCode(){
-  var v_id=event.srcElement.id;
-  fold=v_id.replace("fold_","code_");
-  if(!codeValue)
-  {
-    codeValue = $('#'+fold).html();
-  }
-  if ($("#"+v_id).attr("value")== "展开代码") {
-                  $("#"+fold).show();
-                  $("#"+v_id).attr("value", "折叠代码");
-                  var height=$(window).height()*0.75;
-                  if($('#'+fold+' pre').height()>height)
-                  {
-                     $('#'+fold+' pre').css("height",height);
-                     $('#'+fold+' pre').css("overflow",'auto');
-                  }
-                    
-                  //针对手机浏览器的优化
-                  if(window.screen.width <= 770){
-                    var wid = $(window).width();
-                    $('figure').css("margin-left",wid*0.04);
-                    $('figure').width(wid*0.8);
-                  }
-                 
-              }
-              else {
-                  $("#"+fold).hide();
-                  $("#"+v_id).attr("value", "展开代码");
-              } 
-};
-
-<!--代码窗口和代码折叠结束-->
+<!--代码窗口和代码折叠结束-->>
 
