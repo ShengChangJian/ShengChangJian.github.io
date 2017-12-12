abce01ced0e7ffd50b8d08d8370702937aaaa369
ShengChangJian <414834682@qq.com>
2017-12-12 15:07:55 +0800

    at 2017/12/12 周二 15:07:49.96

--- assets/js/jquery.toc.js
+++ assets/js/jquery.toc.js
@@ -57,14 +57,13 @@ function runCode(){
     codeValue +='<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0">';
     codeValue +='<title>代码窗口</title>';
     codeValue +='<link rel="stylesheet" href="/assets/js/google-code-prettify/prettify.css">';
-    codeValue +='<script src="/assets/js/google-code-prettify/prettify.js">'+'</'+'script>';
     codeValue +='<style>code{width: 100%; margin: 0; padding-top: 10px; }body{font-size: 1em;background-color: #2F4F4F;}</style>';
     codeValue +='<style>li{font-size:1.2em;border-left:2px solid green;text-indent: 1em;} li.L0, li.L1, li.L2, li.L3,li.L5, li.L6, li.L7, li.L8{ list-style-type: decimal !important }</style>'; 
 
     if(window.screen.width <= 770){
         codeValue+='<style>html{overflow: auto !important; -webkit-overflow-scrolling: touch !important;}li{font-size: 1em;} *{margin: 0; padding: 0;} ol.linenums{ padding-top: 1em; padding-left: 2.2em; width: 100%; height: 100%;}</style>';
         codeValue+= '</head><body style=" position: absolute;">' + $('#'+copy).html();
-        codeValue+='<script>document.getElementByTagName("pre").style.height = document.documentElement.clientHeight +"px";</script>'
+        codeValue+='<script>document.getElementByTagName("pre").style.height = window.screen.height; </script>'
         
     }else{
         codeValue+= '</head><body>' + $('#'+copy).html();        
@@ -72,6 +71,7 @@ function runCode(){
   }
   var rng = window.open('','codeWin_'+i, 'height=400, width=780, top=100,left=100, toolbar=no, menubar=no, scrollbars=yes, resizable=yes,location=no,status=no');
       rng.opener=null;
+      codeValue+='<script src="/assets/js/google-code-prettify/prettify.js">'+'</'+'script>';
       codeValue+='</body></html>';
 
       rng.document.write(codeValue);
