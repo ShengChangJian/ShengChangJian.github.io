5af53f9efd17810030263da5a3dff06de884d5a7
shengchangjian <shentchangjian@yy.com>
2021-04-24 15:38:50 +0800

    at 2021年 04月 24日 星期六 15:38:50 CST

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #jekyll clean && git pull origin master && git add . && git commit * -m "at $(date)" && git push origin master
 #git pull origin master && 
-jekyll clean & git add -A && git commit -am "at $(date)" && git push -u origin master;git push -u ShengChangJian master && git push https://github.com/ShengChangJian/ShengChangJian.github.io.git
+jekyll clean & git add -A && git commit -am "at $(date)" && git push -u origin master;git push -u ShengChangJian origin master && git push https://github.com/ShengChangJian/ShengChangJian.github.io.git
 #先同步远程文件，后面的参数会自动连接你远程的文件
 #git pull origin master
 #添加远程不存在的git文件
